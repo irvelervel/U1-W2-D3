069122fd4449346db068cf6588561d7795ff6287
Stefano Casasola <ste.casasola@gmail.com>
2023-05-31 10:53:55 +0200

    starts the for loop explanation

---
+++ for.js
@@ -0,0 +1,22 @@
+// CICLO FOR
+// ...largamente quello più usato.
+// Permette di eseguire un blocco di istruzioni un numero PREDETERMINATO di volte.
+
+// SINTASSI DEL CICLO FOR
+// for(inizializzazione del contatore; condizione da controllare per ripetere l'esecuzione; incremento del contatore){
+//      blocco di istruzioni da eseguire
+// }
+
+// for (let i = 0; i < 10; i++) {
+//   console.log(i)
+//   console.log('esecuzione del ciclo')
+// } // da qua in poi i muore :(
+
+// // i qua non è visibile!
+// console.log('CICLO FINITO')
+
+for (let i = 0; i < 10; i++) {
+  console.log(i)
+}
+
+// il ciclo for è PERFETTO per ESPLORARE GLI ARRAY
